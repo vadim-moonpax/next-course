c63b7cae05a02d18c314290285de31b4efeabf73
moonpax <vadim.mityagin@gmail.com>
2020-09-27 21:06:47 +0300

    my commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ export default function App({ Component, pageProps }) {
     <>
       <NextNprogress
         color="yellow"
-        startPosition="0.3"
-        stopDelayMs="200"
-        height="2"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={2}
       />
       <Component {...pageProps} />
       {/* <style jsx global>
